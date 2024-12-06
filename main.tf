8e2f32682b4de1036831d843397f65dd90fa6c2a
Marek Pastierik <marekpastierik@MacBook-Air-uzivatela-Marek.local>
2024-12-06 23:26:07 +0100

    Task #1: Discovery latest AMI dynamically

--- main.tf
+++ main.tf
@@ -1,5 +1,27 @@
+data "aws_ami" "ubuntu_24_04" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  owners = ["099720109477"] #canonical
+}
+
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-0e9085e60087ce171"
+  ami           = data.aws_ami.ubuntu_24_04.id
   instance_type = "t2.micro"
   user_data     = file("${path.module}/files/hello_world.txt")
 
