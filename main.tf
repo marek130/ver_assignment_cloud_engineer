6363400e2fba7544ab1588819ff1e07f9925971f
Marek Pastierik <marekpastierik@MacBook-Air-uzivatela-Marek.local>
2024-12-06 22:56:14 +0100

    Task #1: Add hello-world.txt feature

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "app_server" {
   ami           = "ami-0e9085e60087ce171"
   instance_type = "t2.micro"
+  user_data     = file("${path.module}/files/hello_world.txt")
 
   tags = {
     Name = var.instance_name
